d0d85338f71d5e3c70cd371906f54375c7f98d4b
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 12:41:16 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("Script running...")
+// Task 1: Light & Dark Mode
+// Select the '☀️' button
+
+
+// Select the '🌑' button
+
+
+// Select the body element
+// You can use document.body or document.querySelector("body")
+
+
+// Add an event listener to the ☀️ button such that when it's clicked, the body's background color is lightyellow
+
+
+
+// Add an event listener to the ☀️ button such that when it's clicked, the body's background color is navy
+// Bonus: also make the body's text color white
+
+
+
+// Task 2: Image size
+// Select the image
+
+
+// Add an event listener to the image such that when it's clicked, the width of the image becomes 110%
+
+
+
+// Task 3: RGB
+// Select the red h2
+
+
+// Add an event listener to the h2 such that when it's clicked, the text color changes to red
+
+
+
+// Implement the same DOM manipulation flows for green and blue h2s
+
+
